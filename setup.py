ed24d0187c643595881a119dcf898a08f5561a9a
Artem Lomonosov <mcproger7@gmail.com>
2020-05-04 22:38:49 +0300

    Update setup file

--- setup.py
+++ setup.py
@@ -1,11 +1,10 @@
-import os
-import codecs
+
 from setuptools import setup
 
 
-def read(fname):
-    file_path = os.path.join(os.path.dirname(__file__), fname)
-    return codecs.open(file_path, encoding='utf-8').read()
+def get_long_description(fname) -> str:
+    with open('README.md') as f:
+        return f.read()
 
 
 setup(
@@ -16,22 +15,17 @@ setup(
     maintainer='Artem Lomonosov',
     maintainer_email='mcproger7@gmail.com',
     license='MIT',
-    url='https://github.com/best-doctor/pytest-network',
+    url='https://github.com/best-doctor/pytest_network',
     description='A simple plugin to disable network on socket level.',
-    long_description=read('README.md'),
+    long_description=get_long_description(),
     py_modules=['pytest_network'],
     install_requires=['pytest>=5.3.1'],
     classifiers=[
-        'Development Status :: 4 - Beta',
         'Framework :: Pytest',
-        'Intended Audience :: Developers',
         'Topic :: Software Development :: Testing',
         'Programming Language :: Python',
+        'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: Implementation :: CPython',
-        'Programming Language :: Python :: Implementation :: PyPy',
-        'Operating System :: OS Independent',
-        'License :: OSI Approved :: MIT License',
     ],
     entry_points={
         'pytest11': [
