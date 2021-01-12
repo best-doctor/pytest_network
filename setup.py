07423f96288cf09141a7316d240aa1b07771577d
Vladislav Katolyk <quadx.vlad@gmail.com>
2021-01-12 10:22:23 +0300

    Add python 3.9 support (#9)

--- setup.py
+++ setup.py
@@ -29,6 +29,7 @@ setup(
         'Programming Language :: Python',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
     ],
     entry_points={
         'pytest11': [
